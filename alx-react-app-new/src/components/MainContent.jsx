879622b9c331c4f80fccc75bf72c471ed9bb2488
Mark Baiden <markbaiden17@gmail.com>
2026-01-18 10:34:40 +0000

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+  return (
+    <main>
+      <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+        <p style={{ fontWeight: 'light', fontStyle: 'italic' }}>
+          I love to visit New York, Paris, and Tokyo.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
