2a9b8277aa0f292fd3ce04d4b4b7e59bca81e954
Mark Baiden <markbaiden17@gmail.com>
2026-01-26 19:14:59 +0000

    Setting Up a React Project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to ="/">Home</Link> | <Link to ="/about">About</Link>
+      </nav>
+
+      <hr />
+
+      <Routes>
+        <Route path ="/" element ={<h1>Home Page</h1>} />
+        <Route path ="/about" element ={<h1>About Page</h1>} />
+      </Routes>
+
+      <hr />
+
+      <footer>&copy; GitHub User Search</footer>
+    </BrowserRouter>
+  );
+}
+
+export default App;
