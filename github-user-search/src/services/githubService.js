f3f0caed81786f5116a348234489cddf8e972ba0
Mark Baiden <markbaiden17@gmail.com>
2026-01-28 09:27:04 +0000

    Edit githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,13 +3,18 @@ import axios from "axios";
 // Get API key from environment variables
 const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-export const fetchUserData = async (username, location, minRepos, page = 1) => {
-    let query = `q=${username}`;
+export const fetchUserData = async (username, location, minRepos) => {
+    let query = `${username}`;
     if (location) query += `+location:${location}`;
     if (minRepos) query += `+repos:>=${minRepos}`;
 
-    const url = `https://api.github.com/search/users?${query}&page=${page}&per_page=10`;
+    const url = `https://api.github.com/search/users?q=${query}`;
     const headers = API_KEY ? { Authorization: `Bearer ${API_KEY}` } : {};
-    const response = await axios.get(url, { headers });
-    return response.data;
+    try {
+        const response = await axios.get(url, { headers });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
 };
